4fef80125a4d7547b7940aa5411787479b14fd94
FusionSid <siddheshadsv@icloud.com>
2022-07-20 20:36:58 +1200

    Made new table for Rooms and function to create it

--- src/core/database/tables.py
+++ src/core/database/tables.py
@@ -17,18 +17,37 @@ async def create_user_db():
         async with conn.transaction():  # start a transaction incase something goes wrong
             await conn.execute(
                 """CREATE TABLE IF NOT EXISTS Users (
-                    user_id INTEGER PRIMARY KEY, 
+                    user_id INTEGER PRIMARY KEY NOT NULL, 
                     username VARCHAR(64) NOT NULL, 
                     email VARCHAR(64) NOT NULL, 
                     password VARCHAR(64) NOT NULL,
                     permission_level INTEGER NOT NULL DEFAULT 1,
-                    public_key TEXT
+                    public_key TEXT NOT NULL,
+                )"""
+            )
+
+
+async def create_room_db():
+    """
+    Creates Rooms table in the database
+
+    Takes no parameters and doesn't return anything
+    """
+    async with asyncpg_connect() as conn:  # connect
+        async with conn.transaction():  # start a transaction incase something goes wrong
+            await conn.execute(
+                """CREATE TABLE IF NOT EXISTS Rooms (
+                    room_id INTEGER PRIMARY KEY NOT NULL, 
+                    room_name VARCHAR(42) NOT NULL, 
+                    room_created_at INTEGER NOT NULL,
+                    room_description TEXT
                 )"""
             )
 
 
 async def main():
     await create_user_db()
+    await create_room_db()
 
 
 asyncio.run(main())

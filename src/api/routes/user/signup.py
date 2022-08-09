1f7a1c9d446dff1437ff44254e2346782c7e03a6
FusionSid <siddheshadsv@icloud.com>
2022-08-10 00:31:54 +1200

    Made username/email conflict search case sensitive so for example if someone has a username of FusionSid no one can now make one called fUsIOnSiD

--- src/api/routes/user/signup.py
+++ src/api/routes/user/signup.py
@@ -36,43 +36,50 @@ async def create_account(
     Create a new user. Used when someone is signing up to the app.
 
     Parameters:
-        username (str): The new users username
-        email (str): The new users email
-        password (str): The new users password
-        public_key (str): The new users public_key
+        username (str): The new users username  
+        email (str): The new users email  
+        password (str): The new users password  
+        public_key (str): The new users public_key  
     """
 
-    user = await get_user(
-        user_data.username
-    )  # Check if someone has the same username/account already exists
+    # user = await get_user(
+    #     user_data.username
+    # )  # Check if someone has the same username/account already exists
 
-    if user is not None:
-        raise HTTPException(
+    # if user is not None:
+    username_conflict_error = HTTPException(
             status_code=409,
             detail="User with this username already exists",
         )
 
-    async with asyncpg_connect() as conn:
-        data = await conn.fetch("SELECT * FROM Users WHERE email=$1", user_data.email)
-        if len(data) != 0:
-            raise HTTPException(
+    email_conflict_error = HTTPException(
                 status_code=409,
                 detail="User with this email already exists",
             )
 
-    # Hash their password (to be stored)
-    hashed_password = await hash_text(user_data.password)
-
     async with asyncpg_connect() as conn:
+        email_search = await conn.fetch("SELECT * FROM Users WHERE LOWER(email)=LOWER($1)", user_data.email)
+        if len(email_search):
+            raise email_conflict_error
+        
+        username_search = await conn.fetch("SELECT * FROM Users WHERE LOWER(username)=LOWER($1)", user_data.username)
+        if len(username_search):
+            raise username_conflict_error
+
+        # Hash their password (to be stored)
+        hashed_password = await hash_text(user_data.password)
+
+
+        user_id = None
+
+        # Keep generating user ids and check if user with id already exists, if not break
+        while True:  
+            user_id = random.randint(0, 1_000_000_000)
+            data = await conn.fetch("SELECT * FROM Users WHERE user_id=$1", user_id)
+            if not len(data):
+                break
+
         async with conn.transaction():
-            user_id = None
-            while (
-                True
-            ):  # Keep generating user ids and check if user with id already exists, if not break
-                user_id = random.randint(0, 1_000_000_000)
-                data = await conn.fetch("SELECT * FROM Users WHERE user_id=$1", user_id)
-                if len(data) == 0:
-                    break
             # insert user into database
             await conn.execute(
                 """INSERT INTO Users (
@@ -88,9 +95,10 @@ async def create_account(
     user = await get_user(user_data.username)  # fetch user from database
     if user is not None:  # if user exists (user was created properly), return user
         del user.password
-        return {"success": True, "detail": "User created successfully", "user": user}
+        return {"successful": True, "detail": "User created successfully", "user": user}
+
     # if not tell em you failed
     raise HTTPException(
         status_code=500,
-        detail="internal error lmao, user failed to be created. Maybe try again",
+        detail={"successful": False, "detail": "internal error lmao, user failed to be created. Maybe try again"}
     )

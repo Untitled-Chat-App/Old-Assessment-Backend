7700775834a7c567be36d73cc6ff8404a397fbd7
FusionSid <siddheshadsv@icloud.com>
2022-08-14 17:32:39 +1200

    forgot a continue

--- src/api/routes/rooms/chatroom_ws.py
+++ src/api/routes/rooms/chatroom_ws.py
@@ -75,6 +75,7 @@ async def connect_ws(websocket: WebSocket, access_token: str, room_id: int):
                 message: RoomMessage = await process_message_json(data, room)
             except json.decoder.JSONDecodeError:
                 await connection.websocket.send_text("skill issue ngl only json idiot")
+                continue
             
             await chatroom.broadcast(message)
 
